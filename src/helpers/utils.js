3e5082aad78f649d89ca693f8fd0fe4e8d5568e4
DanMad <hooky.mounted_0n@icloud.com>
2025-02-09 22:51:55 +1100

    Removed file extensions from import statements

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone.js';
-import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import isEqual from 'lodash/isEqual';
 
 dayjs.extend(timezone);

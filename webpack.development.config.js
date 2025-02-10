83b908ef8cbcb0a9781aca805e758214e6b859b6
DanMad <hooky.mounted_0n@icloud.com>
2025-02-10 15:57:49 +1100

    Refactored Webpack Configuration

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DotenvWebpack = require('dotenv-webpack');
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.config.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { version } = require('./package.json');
 
-module.exports = merge(commonConfig, {
+module.exports = {
   devServer: {
     compress: true,
-    historyApiFallback: {
-      rewrites: [{ from: /./, to: '404.html' }],
-    },
+    historyApiFallback: true,
     hot: true,
     open: true,
     port: 3000,
@@ -17,9 +16,28 @@ module.exports = merge(commonConfig, {
     },
   },
   devtool: 'inline-source-map',
+  entry: './src/index.jsx',
   mode: 'development',
   module: {
     rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic',
+                },
+              ],
+            ],
+          },
+        },
+      },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
@@ -28,8 +46,38 @@ module.exports = merge(commonConfig, {
     ],
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/assets',
+          to: path.join(__dirname, 'dist/assets'),
+        },
+      ],
+    }),
     new DotenvWebpack({
       path: './.env.development',
     }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './public/index.html',
+      templateParameters: {
+        description:
+          'A weather app that displays the forecast, current conditions, and UV Index to help you stay sun-safe.',
+        image: {
+          alt: 'An exhibit of a weather app displaying the forecast, current conditions, and UV Index.',
+          src: '/assets/thumbnail.svg',
+        },
+        isDevelopment: true,
+        isIndexed: true,
+        referrer: 'unsafe-url',
+        title: 'The Weather',
+        url: 'https://weather.danielmaddison.io',
+        version,
+      },
+    }),
   ],
-});
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    modules: ['./src', 'node_modules'],
+  },
+};
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,33 @@
 const path = require('path');
-const CnameWebpackPlugin = require('cname-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DotenvWebpack = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.config.js');
+const { version } = require('./package.json');
 
-module.exports = merge(commonConfig, {
+module.exports = {
+  entry: './src/index.jsx',
   mode: 'production',
   module: {
     rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic',
+                },
+              ],
+            ],
+          },
+        },
+      },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
@@ -24,17 +43,64 @@ module.exports = merge(commonConfig, {
   output: {
     clean: true,
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
   },
   plugins: [
-    new CnameWebpackPlugin({
-      domain: 'weather.danielmaddison.io',
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/assets',
+          to: path.join(__dirname, 'dist/assets'),
+        },
+        { from: 'public/CNAME', to: path.join(__dirname, 'dist') },
+      ],
     }),
     new DotenvWebpack({
       path: './.env.production',
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './public/index.html',
+      templateParameters: {
+        description:
+          'A weather app that displays the forecast, current conditions, and UV Index to help you stay sun-safe.',
+        image: {
+          alt: 'An exhibit of a weather app displaying the forecast, current conditions, and UV Index.',
+          src: '/assets/thumbnail.svg',
+        },
+        isDevelopment: false,
+        isIndexed: true,
+        referrer: 'strict-origin-when-cross-origin',
+        title: 'The Weather',
+        url: 'https://weather.danielmaddison.io',
+        version,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      filename: '404.html',
+      inject: 'body',
+      template: './public/index.html',
+      templateParameters: {
+        description:
+          'A weather app that displays the forecast, current conditions, and UV Index to help you stay sun-safe.',
+        image: {
+          alt: 'An exhibit of a weather app displaying the forecast, current conditions, and UV Index.',
+          src: '/assets/thumbnail.svg',
+        },
+        isDevelopment: false,
+        isIndexed: false,
+        referrer: 'strict-origin-when-cross-origin',
+        title: 'Page not found',
+        url: 'https://weather.danielmaddison.io',
+        version,
+      },
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
   ],
-});
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    modules: ['./src', 'node_modules'],
+  },
+};
